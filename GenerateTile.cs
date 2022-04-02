271fecd54bc19a806c8401168e4e3b1201f31b1f
ZXC TESLA <87894848+TeslaDevQ@users.noreply.github.com>
2022-04-02 12:59:51 +0300

    Add files via upload

---
+++ GenerateTile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTile : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    private List<GameObject> activeTiles = new List<GameObject>();
+    private float spawnPos = 0;
+    public float tileLength = 100;
+
+
+    [SerializeField] private Transform player;
+    private int startTiles = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < startTiles; i++)
+        {
+            if(i == 0)
+            {
+                SpawnTile(0);
+            }
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.y - 40 > spawnPos - (startTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    private void SpawnTile(int tileIndex)
+    {
+        GameObject nextTile = Instantiate(tilePrefabs[tileIndex], transform.up * spawnPos, transform.rotation);
+        activeTiles.Add(nextTile);
+        spawnPos += tileLength;
+    }
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
